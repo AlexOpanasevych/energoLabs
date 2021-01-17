e45d959a073fb92e8c63f6914c1e14dca9587527
dimon836 <65618206+dimon836@users.noreply.github.com>
2021-01-17 05:25:15 +0200

    add methods
    add "get" and "insert" methods to DataProvider

--- energoLabs/dataprovider.cpp
+++ energoLabs/dataprovider.cpp
@@ -3,6 +3,7 @@
 #include <QRandomGenerator>
 #include <QVariantMap>
 #include <QDebug>
+#include <string>
 
 DataProvider::DataProvider(QObject *parent) : QObject(parent), p("../../energoLabs/lab3.db")
 {
@@ -23,4 +24,113 @@ QVariantList DataProvider::thirdGraphData(int dayFirst, int daySecond)
         qDebug() << map;
     }
     return result;
+}
+
+QList<QVariantMap> get_electricity_dayOfWeek()
+{
+    p.setTable("electricity_dayofweek");
+    return p.get;
+}
+
+QList<QVariantMap> get_electricity_electricalDevices()
+{
+    p.setTable("electricity_electricaldevices");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_projectZone()
+{
+    p.setTable("electricity_projectzone");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_switchType()
+{
+    p.setTable("electricity_switchtype");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_tariff()
+{
+    p.setTable("electricity_tariff");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_tariffRange()
+{
+    p.setTable("electricity_tariffrange");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_tariffZone()
+{
+    p.setTable("electricity_tariffzone");
+    return p.get();
+}
+
+QList<QVariantMap> get_electricity_userDevice()
+{
+    p.setTable("electricity_userdevice");
+    return p.get();
+}
+
+bool insert_electricity_dayOfWeek(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_dayofweek");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_electricalDevices(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_electricaldevices");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_projectZone(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_projectzone");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_switchType(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_switchtype");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_tariff(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_tariff");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_tariffRange(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_tariffrange");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_tariffZone(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_tariffzone");
+    return p.insertRow(values);
+}
+
+bool insert_electricity_userDevice(QMap<QString, QVariant> & values)
+{
+    p.setTable("electricity_userdevice");
+    return p.insertRow(values);
+}
+
+
+QList<QVariantMap> DataProvider::getTableInfo(const std::string nameTable)
+{
+    p.setTable(nameTable);
+    return p.get();
+}
+
+bool DataProvider::insertInfo(const std::string nameTable, const QMap<QString, QVariant> & values)
+{
+    p.setTable(nameTable);
+    return p.insertRow(values);
 }
--- energoLabs/dataprovider.h
+++ energoLabs/dataprovider.h
@@ -4,6 +4,7 @@
 #include <QObject>
 //#include <kmacro.h>
 #include <sqliteprovider.h>
+#include <string>
 
 class DataProvider : public QObject
 {
@@ -16,6 +17,26 @@ public:
 public slots:
     QVariantList thirdGraphData(int dayFirst, int daySecond);
 
+    QList<QVariantMap> get_electricity_dayOfWeek();
+    QList<QVariantMap> get_electricity_electricalDevices();
+    QList<QVariantMap> get_electricity_projectZone();
+    QList<QVariantMap> get_electricity_switchType();
+    QList<QVariantMap> get_electricity_tariff();
+    QList<QVariantMap> get_electricity_tariffRange();
+    QList<QVariantMap> get_electricity_tariffZone();
+    QList<QVariantMap> get_electricity_userDevice();
+
+    bool insert_electricity_dayOfWeek(QMap<QString, QVariant> & values);
+    bool insert_electricity_electricalDevices(QMap<QString, QVariant> & values);
+    bool insert_electricity_projectZone(QMap<QString, QVariant> & values);
+    bool insert_electricity_switchType(QMap<QString, QVariant> & values);
+    bool insert_electricity_tariff(QMap<QString, QVariant> & values);
+    bool insert_electricity_tariffRange(QMap<QString, QVariant> & values);
+    bool insert_electricity_tariffZone(QMap<QString, QVariant> & values);
+    bool insert_electricity_userDevice(QMap<QString, QVariant> & values);
+
+    QList<QVariantMap> getTableInfo(std::string nameTable);
+    bool insertInfo(std::string nameTable, QMap<QString, QVariant> & values);
 
 signals:
 
